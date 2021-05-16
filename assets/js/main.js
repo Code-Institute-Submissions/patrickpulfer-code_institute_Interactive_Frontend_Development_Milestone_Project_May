48c4f43aeb8eb55686e02b55316092acefd87cb4
patrickpulfer <patrick.pulfer1@gmail.com>
2021-05-16 15:18:11 +0100

    Update: Update source API data to filter out continents
    Recent changes introduced continents to the source API for covid data. Continents have been filtered out

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,42 +2,42 @@
 /*globals $:false, google */
 
 // Global Variables
-    var latestbyCountry = Array();
-    var totalbyCountry = Array();
-    var newsAPI = Array();
-    var newsAPI2 = Array();
-    var totalDeaths;
-    var totalCases;
-    var covidDataTimestamp = Array();
-    var covidLatestTableData = Array();
-    var covidTotalTableData = Array();
-    
+var latestbyCountry = Array();
+var totalbyCountry = Array();
+var newsAPI = Array();
+var newsAPI2 = Array();
+var totalDeaths;
+var totalCases;
+var covidDataTimestamp = Array();
+var covidLatestTableData = Array();
+var covidTotalTableData = Array();
+
 
 // Importing Google Charts, then run API calls
-    google.charts.load('current', {
-        'packages':['geochart','corechart'],
-        'mapsApiKey': 'AIzaSyAz5bDiEFM6Ugta6CMOcMj6f3m55A16p3w'
-    }).then(getCovidLatestData);
+google.charts.load('current', {
+    'packages': ['geochart', 'corechart'],
+    'mapsApiKey': 'AIzaSyAz5bDiEFM6Ugta6CMOcMj6f3m55A16p3w'
+}).then(getCovidLatestData);
 
 
-$(document).ready(function(){
+$(document).ready(function () {
     getCovidNews();
     getCovidDataTimestamp();
 
-    $(".toggle_stats_button").click(function(){
+    $(".toggle_stats_button").click(function () {
         $("#covid_total_map").parent().parent().slideToggle("slow");
         $("#covid_latest_map").parent().parent().slideToggle("slow");
     });
 
-    $(".switch_total_map").click(function(){
-        if ($("#covid_latest_map").is(':visible')){
+    $(".switch_total_map").click(function () {
+        if ($("#covid_latest_map").is(':visible')) {
             $("#covid_total_map").parent().parent().slideToggle("slow");
             $("#covid_latest_map").parent().parent().slideToggle("slow");
         }
     });
 
-    $(".switch_latest_map").click(function(){
-        if ($("#covid_total_map").is(':visible')){
+    $(".switch_latest_map").click(function () {
+        if ($("#covid_total_map").is(':visible')) {
             $("#covid_total_map").parent().parent().slideToggle("slow");
             $("#covid_latest_map").parent().parent().slideToggle("slow");
         }
@@ -46,120 +46,126 @@ $(document).ready(function(){
 
 });
 
-function getCovidLatestData(){
+function getCovidLatestData() {
     var endpoint = 'https://covid.ourworldindata.org/data/latest/owid-covid-latest.json';
     $.ajax({
         url: endpoint,
         dataType: 'json',
-        success: function(result){
+        success: function (result) {
             latestbyCountry.push(['Country', 'New Cases', 'New Deaths']);
             totalbyCountry.push(['Country', 'Total Cases', 'Total Deaths']);
 
             // Parse Statistics
-                $.each(result, function(i, val){
-                    if(result[i].location === 'World'){return;}
-                    latestbyCountry.push([(result[i].location),(result[i].new_cases),(result[i].new_deaths)]);
-                    totalbyCountry.push([(result[i].location),(result[i].total_cases),(result[i].total_deaths)]);
-                    covidLatestTableData.push([(result[i].location),(result[i].new_cases),(result[i].new_deaths),(result[i].new_tests),(result[i].hosp_patients),(result[i].icu_patients),(result[i].positive_rate)]);
-                    covidTotalTableData.push([(result[i].location),(result[i].total_cases),(result[i].total_deaths),(result[i].total_tests),(result[i].total_vaccinations),(result[i].life_expectancy),(result[i].population)]);
-                });
+            $.each(result, function (i, val) {
+                if (result[i].location === 'World' ||
+                    result[i].location === 'Asia' ||
+                    result[i].location === 'South America' ||
+                    result[i].location === 'Europe' ||
+                    result[i].location === 'European Union' ||
+                    result[i].location === 'North America'
+                ) { return; }
+                latestbyCountry.push([(result[i].location), (result[i].new_cases), (result[i].new_deaths)]);
+                totalbyCountry.push([(result[i].location), (result[i].total_cases), (result[i].total_deaths)]);
+                covidLatestTableData.push([(result[i].location), (result[i].new_cases), (result[i].new_deaths), (result[i].new_tests), (result[i].hosp_patients), (result[i].icu_patients), (result[i].positive_rate)]);
+                covidTotalTableData.push([(result[i].location), (result[i].total_cases), (result[i].total_deaths), (result[i].total_tests), (result[i].total_vaccinations), (result[i].life_expectancy), (result[i].population)]);
+            });
 
             // Draw Google GeoChart
-                drawRegionsMap(latestbyCountry, 'covid_latest_map', '#FF7F00');
-                drawRegionsMap(totalbyCountry, 'covid_total_map', '#FF0000');
-                $("#covid_total_map").parent().parent().toggle();
-                drawDailybyCountryTable(covidLatestTableData);
-                drawTotalbyCountryTable(covidTotalTableData);
+            drawRegionsMap(latestbyCountry, 'covid_latest_map', '#FF7F00');
+            drawRegionsMap(totalbyCountry, 'covid_total_map', '#FF0000');
+            $("#covid_total_map").parent().parent().toggle();
+            drawDailybyCountryTable(covidLatestTableData);
+            drawTotalbyCountryTable(covidTotalTableData);
 
             // Display Totals
-                totalCases = result.OWID_WRL.new_cases;
-                totalDeaths = result.OWID_WRL.new_deaths;
-                $('#total_cases').text(new Intl.NumberFormat().format(totalCases));
-                $('#total_deaths').text(new Intl.NumberFormat().format(totalDeaths));
+            totalCases = result.OWID_WRL.new_cases;
+            totalDeaths = result.OWID_WRL.new_deaths;
+            $('#total_cases').text(new Intl.NumberFormat().format(totalCases));
+            $('#total_deaths').text(new Intl.NumberFormat().format(totalDeaths));
 
         },
-        error: function(XMLHttpRequest, textStatus, errorThrown) { 
-            alert("Status: " + textStatus); alert("Error: " + errorThrown); 
-        }    
+        error: function (XMLHttpRequest, textStatus, errorThrown) {
+            alert("Status: " + textStatus); alert("Error: " + errorThrown);
+        }
     });
 }
 
-function getCovidDataTimestamp(){
+function getCovidDataTimestamp() {
     fetch('https://covid.ourworldindata.org/data/owid-covid-data-last-updated-timestamp.txt')
-    .then(res => res.text())
-    .then(data => {
-        covidDataTimestamp.push(data.substr(0,10));
-        covidDataTimestamp.push(data.substr(11,8));
-        $('.last_update_badge').text(covidDataTimestamp[0] + ' ' + covidDataTimestamp[1]);
-    })
+        .then(res => res.text())
+        .then(data => {
+            covidDataTimestamp.push(data.substr(0, 10));
+            covidDataTimestamp.push(data.substr(11, 8));
+            $('.last_update_badge').text(covidDataTimestamp[0] + ' ' + covidDataTimestamp[1]);
+        })
 
-    .catch((error) => {
-        console.error('Error:', error);
-    });
+        .catch((error) => {
+            console.error('Error:', error);
+        });
 }
 
 function drawRegionsMap(dataForDrawing, htmlElement, color) {
     var data = google.visualization.arrayToDataTable(dataForDrawing);
     var options = {
         colors: [(color)],
-        sizeAxis: { minValue: 0, maxValue: 300000}
+        sizeAxis: { minValue: 0, maxValue: 300000 }
     };
     var chart = new google.visualization.GeoChart(document.getElementById(htmlElement));
     chart.draw(data, options);
 }
 
 
-function getCovidNews(){
+function getCovidNews() {
     var url = 'http://api.mediastack.com/v1/news?access_key=6999d5eee97103a6a145cc12f2af7615&keywords=covid&languages=en&limit=100';
-    var req = new Request(url); 
+    var req = new Request(url);
     fetch(req)
-    .then(response => response.json())
-    .then(data => {
-        newsAPI2 = data.data;
-        var i=0;
+        .then(response => response.json())
+        .then(data => {
+            newsAPI2 = data.data;
+            var i = 0;
 
-        // Attempting to filter out duplicating news
-            $.each(newsAPI2, function(i, val){
-                if (i == 49){return false;}
-                if (newsAPI2[i].title != newsAPI2[(i+1)].title){
-                    if (newsAPI2[i].image == null){newsAPI2[i].image = './assets/images/news.jpg'; }
+            // Attempting to filter out duplicating news
+            $.each(newsAPI2, function (i, val) {
+                if (i == 49) { return false; }
+                if (newsAPI2[i].title != newsAPI2[(i + 1)].title) {
+                    if (newsAPI2[i].image == null) { newsAPI2[i].image = './assets/images/news.jpg'; }
                     newsAPI.push(newsAPI2[i]);
                 }
             });
 
-        // Display 6 news items from source
-            for (i=0; i<6; i++){
+            // Display 6 news items from source
+            for (i = 0; i < 6; i++) {
                 $('#carousel_' + i).find("img").attr('src', newsAPI[i].image);
                 $('#carousel_' + i).find(".news_title").text(newsAPI[i].title);
-                $('#carousel_' + i).find(".news_description").text(newsAPI[i].description.substring(0,300));
+                $('#carousel_' + i).find(".news_description").text(newsAPI[i].description.substring(0, 300));
                 $('#carousel_' + i).find("a").attr('href', newsAPI[i].url);
-                $('#carousel_' + i).find(".news_details").append('<i class="fas fa-link"></i>&nbsp;' + newsAPI[i].source + '&nbsp;&nbsp;<i class="far fa-clock"></i>&nbsp;' + newsAPI[i].published_at.substr(11,8) + '&nbsp;&nbsp;<i class="far fa-calendar-alt"></i>&nbsp;' + newsAPI[i].published_at.substr(0,10));
-            } 
+                $('#carousel_' + i).find(".news_details").append('<i class="fas fa-link"></i>&nbsp;' + newsAPI[i].source + '&nbsp;&nbsp;<i class="far fa-clock"></i>&nbsp;' + newsAPI[i].published_at.substr(11, 8) + '&nbsp;&nbsp;<i class="far fa-calendar-alt"></i>&nbsp;' + newsAPI[i].published_at.substr(0, 10));
+            }
 
-        //Error handling when API returns empty data
-            if(newsAPI === '' || newsAPI === undefined || newsAPI === null || newsAPI.length === 0){
+            //Error handling when API returns empty data
+            if (newsAPI === '' || newsAPI === undefined || newsAPI === null || newsAPI.length === 0) {
                 $('#covidNews').hide();
                 $('#covidNews').parent().append('Loading News from MediaStack.com failed. Reload Page to try again.');
             }
-    })
+        })
 
-    .catch((error) => {
-        $('#covidNews').hide();
-        $('#covidNews').parent().append('Loading News from MediaStack.com failed. See Console for Error');
-        console.error('Error:', error);
+        .catch((error) => {
+            $('#covidNews').hide();
+            $('#covidNews').parent().append('Loading News from MediaStack.com failed. See Console for Error');
+            console.error('Error:', error);
+        });
+}
+
+function drawDailybyCountryTable(covidLatestTableData) {
+    $('#fist_table').DataTable({
+        data: covidLatestTableData,
+        "order": [[1, "desc"]],
     });
 }
 
-function drawDailybyCountryTable(covidLatestTableData){
-    $('#fist_table').DataTable( {
+function drawTotalbyCountryTable(covidLatestTableData) {
+    $('#total_table').DataTable({
         data: covidLatestTableData,
-        "order": [[ 1, "desc" ]],
-    });
-}
-
-function drawTotalbyCountryTable(covidLatestTableData){
-    $('#total_table').DataTable( {
-        data: covidLatestTableData,
-        "order": [[ 1, "desc" ]],
+        "order": [[1, "desc"]],
     });
 }
